440d7af41e116a1be7bb07c5db8fd6b905289035
Sojeong <0brilliantsophie1@gmail.com>
2024-12-04 01:18:13 +0900

    섹션 4. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
     - 관심사의 분리
     - AppConfig 리팩터링
     - 새로운 구조와 할인 정책 적용
     - 전체 흐름 정리
     - 좋은 객체 지향 설계와 5가지 원칙의 적용
     - IoC, DI, 그리고 컨테이너
     - 스프링으로 전환하기

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,19 +7,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+//        MemberService memberService = new MemberServiceImpl(null);
+//        OrderService orderService = new OrderServiceImpl(null, null);
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
         // 변수 생성 ctrl + alt + v
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
         // order = Order{memberId=1, itemName='itemA', itemPrice=10000, discountPrice=1000}
