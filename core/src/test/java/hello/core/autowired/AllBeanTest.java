44e66310c6e4757bc11891bb258684059859cd2f
Sojeong Cho <0brilliantsophie1@gmail.com>
2024-12-19 18:44:08 +0900

    섹션 8 진행중

--- core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -1,7 +1,10 @@
 package hello.core.autowired;
 
+import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
@@ -11,16 +14,19 @@ public class AllBeanTest {
 
     @Test
     void findAllBean() {
-        new AnnotationConfigApplicationContext();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
     }
 
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
         private final List<DiscountPolicy> policies;
 
+        @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
             this.policyMap = policyMap;
             this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
         }
     }
 }
