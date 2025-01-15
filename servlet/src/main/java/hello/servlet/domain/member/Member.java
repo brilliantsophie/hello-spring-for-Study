2febbf10a81e2056d7a0815ddd9e5207d9b9ac1c
Sojeong <0brilliantsophie1@gmail.com>
2025-01-15 23:44:14 +0900

    스프링MVC 1편 - 섹션4. 서블릿, JSP, MVC 패턴
     - 회원 관리 웹 애플리케이션 요구사항
     - 서블릿으로 회원 관리 웹 애플리케이션 만들기

--- servlet/src/main/java/hello/servlet/domain/member/Member.java
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -8,8 +8,10 @@ public class Member {
     private Long id;
     private String username;
     private int age;
+
     public Member() {
     }
+
     public Member(String username, int age) {
         this.username = username;
         this.age = age;
