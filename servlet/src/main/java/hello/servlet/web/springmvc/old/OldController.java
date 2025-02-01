cd06164b7e25b9a8a428947faf5bc0044f63ccc2
Sojeong <0brilliantsophie1@gmail.com>
2025-02-01 17:53:35 +0900

    스프링MVC 1편 - 섹션6. 스프링MVC - 구조 이해
     - 스프링 MVC 전체 구조
     - 핸들러 매핑과 핸들러 어댑터
     - 뷰 리졸버

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -1,15 +1,18 @@
 package hello.servlet.web.springmvc.old;
-import org.springframework.stereotype.Component;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
+
     @Override
-    public ModelAndView handleRequest(HttpServletRequest request,
-                                      HttpServletResponse response) throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
