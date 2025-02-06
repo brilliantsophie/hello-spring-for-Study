67124ef3e062c484134e155475f9e4cc54d1efe9
Sojeong <0brilliantsophie1@gmail.com>
2025-02-06 23:55:29 +0900

    스프링 MVC - 기본기능 > 프로젝트 생성

---
+++ springmvn/src/main/java/hello/springmvn/SpringmvnApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvnApplication.class, args);
+	}
+
+}
