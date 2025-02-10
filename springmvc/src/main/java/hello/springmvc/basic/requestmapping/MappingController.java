c6985af5d20b3d73cbd1c1c85b81c40363ea0166
Sojeong <0brilliantsophie1@gmail.com>
2025-02-10 23:06:49 +0900

    스프링 MVC 1편 - 스프링 MVC 기본기능  - 로깅 간단히 알아보기  - 요청 매핑 진행중

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -16,4 +16,28 @@ public class MappingController {
         log.info("helloBasic");
         return "ok";
     }
+
+    /**
+     * method 특정 HTTP 메서드 요청만 허용
+     * GET, HEAD, POST, PUT, PATCH, DELETE
+     */
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    /**
+     * 편리한 축약 애노테이션 (코드보기)
+     * @GetMapping
+     * @PostMapping
+     * @PutMapping
+     * @DeleteMapping
+     * @PatchMapping
+     */
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
 }
