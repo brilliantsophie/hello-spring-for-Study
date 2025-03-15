238311671360217672da3a7127154b0edbdebe0b
Sojeong <0brilliantsophie1@gmail.com>
2025-03-15 23:35:07 +0900

    스프링 MVC1편 - HTTP body에 메세지 HttpServletResponse로 보내줌

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/ResponseBodyController.java
@@ -0,0 +1,19 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+//@RestController
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+}
