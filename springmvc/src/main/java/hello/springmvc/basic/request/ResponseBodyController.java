1a9c443fe2e4304a444d5342ffc45d53ee113152
Sojeong <0brilliantsophie1@gmail.com>
2025-03-17 23:46:19 +0900

    스프링 MVC1편 - ResponseEntity

--- springmvc/src/main/java/hello/springmvc/basic/request/ResponseBodyController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/ResponseBodyController.java
@@ -2,6 +2,8 @@ package hello.springmvc.basic.request;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,5 +17,14 @@ public class ResponseBodyController {
     @GetMapping("/response-body-string-v1")
     public void responseBodyV1(HttpServletResponse response) throws IOException {
         response.getWriter().write("ok");
+    }
+
+    /**
+     * HttpEntity, ResponseEntity(Http Status 추가)
+     * @return
+     */
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 }
